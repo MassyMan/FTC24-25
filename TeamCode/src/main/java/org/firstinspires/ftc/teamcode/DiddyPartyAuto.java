dda41b4a81aea01434e5fcfe9bdb64916eee2291
cader <caderinvader@outlook.com>
2024-12-31 01:56:41 -0500

    Working on auto pid to point

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiddyPartyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiddyPartyAuto.java
@@ -11,12 +11,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 
 @Autonomous(name = "Diddy Party Auto")
 public class DiddyPartyAuto extends LinearOpMode {
 
     ThreeDeadWheelLocalizer localizer;
-    Pose2d pose;
+    Pose2d pose = new Pose2d(0, 0, 0);
     ElapsedTime elapsedTime;
 
     // Initialize Variables
@@ -36,12 +38,12 @@ public class DiddyPartyAuto extends LinearOpMode {
     double x = 0;
     double y = 0;
     double t = 0;
-    double xKP = 0;
-    double xKD = 0;
-    double yKP = 0;
-    double yKD = 0;
-    double tKP = 0;
-    double tKD = 0;
+    double xKP = 0.0001;
+    double xKD = 0.0001;
+    double yKP = 0.0001;
+    double yKD = 0.0001;
+    double tKP = 0.0001;
+    double tKD = 0.0001;
     double x_rotated = 0;
     double y_rotated = 0;
 
@@ -52,16 +54,15 @@ public class DiddyPartyAuto extends LinearOpMode {
 
     // Initialize Servos
     DcMotor extendoEncoder; // REV Robotics through-bore encoder attached to horizontal extension
-    CRServo slidL, slidR;
-    Servo V4Bar, intakeL, intakeR;
+    CRServo slidL, slidR, intakeL, intakeR;
+    Servo V4Bar;
+
+    // Initialize Scheduler
+    private ActionScheduler scheduler;
 
     // ===============================================================================
 
     public void calculatePID(){
-        // Fetch current pose
-        Twist2dDual<Time> twist = localizer.update();
-        pose = pose.plus(twist.value());
-
         // Update current position variables
         currentX = pose.position.x;
         currentY = pose.position.y;
@@ -70,11 +71,15 @@ public class DiddyPartyAuto extends LinearOpMode {
         errorX = currentX - targetX;
         errorY = currentY - targetY;
         errorT = currentT - Math.toRadians(targetT);
+
         // Run PID algorithm based on error
         x = (errorX * xKP) + (((errorX - lastErrorX) / elapsedTime.seconds()) * xKD);
         y = (errorY * yKP) + (((errorY - lastErrorY) / elapsedTime.seconds()) * yKD);
         t = (errorT * tKP) + (((errorT - lastErrorT) / elapsedTime.seconds()) * tKD);
         // Reset Timer
+        lastErrorX = errorX;
+        lastErrorY = errorY;
+        lastErrorT = errorT;
         elapsedTime.reset();
     }
 
@@ -84,8 +89,8 @@ public class DiddyPartyAuto extends LinearOpMode {
         targetY = yPos;
         targetT = heading; // in degrees
 
-        // While the error is larger than the tolerance
-        while (errorX + errorY > tolerance) {
+        // If the error is larger than the tolerance
+        if ((Math.sqrt(Math.pow(currentX - targetX, 2) + Math.pow(currentY - targetY, 2))) > tolerance) {
             // Run PID algorithm
             calculatePID();
 
@@ -98,6 +103,7 @@ public class DiddyPartyAuto extends LinearOpMode {
             leftBack.setPower((x_rotated - y_rotated + t) * (moveSpeed / 100));
             rightFront.setPower((x_rotated - y_rotated - t) * (moveSpeed / 100));
             rightBack.setPower((x_rotated + y_rotated - t) * (moveSpeed / 100));
+
         }
     }
 
@@ -109,11 +115,18 @@ public class DiddyPartyAuto extends LinearOpMode {
 
     }
 
+    private void slidesSpecimen() {
+        telemetry.addData("Running intakeSample . . .", "");
+        telemetry.update();
+        moveSlides(500, 1, true);
+    }
+
     // ===============================================================================
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        elapsedTime = new ElapsedTime();
         // Initialize Drivetrain
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
@@ -136,14 +149,12 @@ public class DiddyPartyAuto extends LinearOpMode {
         slidL = hardwareMap.get(CRServo.class, "slidL");
         slidR = hardwareMap.get(CRServo.class, "slidR");
         V4Bar = hardwareMap.get(Servo.class, "v4Bar");
-        intakeL = hardwareMap.get(Servo.class, "intakeL");
-        intakeR = hardwareMap.get(Servo.class, "intakeR");
-
+        intakeL = hardwareMap.get(CRServo.class, "intakeL");
+        intakeR = hardwareMap.get(CRServo.class, "intakeR");
 
         // Initialize Hang
         wormL = hardwareMap.get(DcMotor.class, "wormL");
         wormR = hardwareMap.get(DcMotor.class, "wormR");
-
 
         // Initialize RR1.0 Localizer
         localizer = new ThreeDeadWheelLocalizer(hardwareMap, 0.0019547157517511);
@@ -152,19 +163,36 @@ public class DiddyPartyAuto extends LinearOpMode {
         waitForStart();
 
         // STARTING POSITION
-        Pose2d pose = new Pose2d(0, 0, Math.toRadians(90));
+
+        // Initialize sequential actions
+        scheduler = new ActionScheduler();
+
+        scheduler.schedule(1, this::slidesSpecimen);
 
         // Loop
         while (opModeIsActive()) {
-
+            Twist2dDual<Time> twist = localizer.update();
+            pose = pose.plus(twist.value());
 
             telemetry.addData("Running DiddyPartyAuto", "");
+            telemetry.addData("Current X", currentX);
+            telemetry.addData("Current Y", currentY);
+            telemetry.addData("Current T", currentT);
+            telemetry.addData("Target X", targetX);
+            telemetry.addData("Target Y", targetY);
+            telemetry.addData("Target T", targetT);
+            telemetry.addData("x", x);
+            telemetry.addData("y", y);
+            telemetry.addData("t", t);
+            telemetry.addData("Version ", "110");
             telemetry.update();
 
 
-            pidToPoint(-10, -10, 50, 100, 3, 0);
+          //  scheduler.runScheduled();
 
-            break;
+            pidToPoint(10, 10, 0, 60, 2, 0);
+
+
 
 
         }
